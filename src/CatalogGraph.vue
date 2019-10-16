79f2fb32ca3fe3bf956c2f259394c5bfc3f00560
ludanxer <haoran.sun@sap.com>
2019-10-16 18:00:43 +0800

    WIP: To Be Continued

--- src/CatalogGraph.vue
+++ src/CatalogGraph.vue
@@ -5,11 +5,32 @@
 </template>
 
 <script>
+import { forceSimulation } from 'd3-force'
+import GraphNode from './graphNode'
+// Demo: https://observablehq.com/@d3/force-directed-graph?collection=@d3/d3-force
 export default {
   name: 'catalog-graph',
-  mounted(){
+  data(){
+    return{
+      root: undefined
+    }
+  },
+  // mounted(){
+  //   const { pages } = this.$site
+  //   console.log(pages)
+  // },
+  created(){
+    // Parse the whole pages to an array of GraphNodes, and find the root node
+    console.log(forceSimulation)
     const { pages } = this.$site
-    console.log(pages)
+    console.log(this.$page)
+    for(let page of pages){
+      let newNode = new GraphNode(page)
+      // console.log(newNode)
+    }
+  },
+  computed: {
+
   }
 }
 </script>
