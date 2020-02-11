1621a48888f22130c74f1f587d3836297fb26914
ludanxer <ludanxer@gmail.com>
2020-02-11 15:58:01 +0800

    remove demo data

--- src/enhanceAppFile.js
+++ src/enhanceAppFile.js
@@ -1,14 +1,5 @@
 import CatalogGraph from './CatalogGraph.vue'
-import demoData from './demo.json'
 
 export default ({ Vue }) => {
   Vue.component('CatalogGraph', CatalogGraph);
-
-  Vue.mixin({
-    computed: {
-      $demoData: function() {
-        return demoData
-      }
-    }
-  });
 }
