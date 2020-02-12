956373368c7afe32b758517c6532a943d72dad40
ludanxer <ludanxer@gmail.com>
2020-02-12 22:16:03 +0800

    feat: pass on user configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const path = require('path')
 
-module.exports = () => ({
+// Stringify functions
+replacer = (key, value) => {
+  return (typeof value === 'function') ? value.toString() : value;
+}
+
+module.exports = (options) => ({
   name: 'vuepress-plugin-catalog-graph',
-  enhanceAppFiles: path.resolve(__dirname, 'enhanceAppFile.js')
+  enhanceAppFiles: [
+    path.resolve(__dirname, 'enhanceAppFile.js'),
+    { // There maybe a better way to pass on users' configurations
+      name: 'catalog-graph-enhancer',
+      content: `
+      export default ({ Vue }) => {
+        Vue.prototype.$catalogGraph = ` + JSON.stringify(options, replacer).replace(/\"/g, "") + `
+      }`.trim()
+    }
+  ]
 })
