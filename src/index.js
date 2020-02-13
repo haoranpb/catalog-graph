741c283e12693a88a3b4f4cb9ce3142c684c6861
ludanxer <ludanxer@gmail.com>
2020-02-13 12:23:18 +0800

    feat: option type check

--- src/index.js
+++ src/index.js
@@ -5,8 +5,21 @@ replacer = (key, value) => {
   return (typeof value === 'function') ? value.toString() : value;
 }
 
+const optionTypes = ['function'];
+
 module.exports = (options) => ({
   name: 'vuepress-plugin-catalog-graph',
+  async ready() {
+    // Check for configuration before continue
+    for(let field in options) {
+      for(let value of Object.values(options[field])) {
+        if(!optionTypes.includes(typeof value)) {
+          console.log('Your configuration for plugin catalog-graph has failed!');
+          process.exit(1);
+        }
+      }
+    }
+  },
   enhanceAppFiles: [
     path.resolve(__dirname, 'enhanceAppFile.js'),
     { // There maybe a better way to pass on users' configurations
