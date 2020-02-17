4330ec524c722c3f4b12cadbfd7370cb129586cc
ludanxer <ludanxer@gmail.com>
2020-02-17 18:50:56 +0800

    docs: update color for nodes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,27 @@ module.exports = {
     ['@vuepress/google-analytics', {
       'ga': 'UA-146772217-3'
     }],
-    require('../../lib/index.js'),
+    [require('../../lib/index.js'), {
+      node: {
+        color: d => {
+          switch(true) {
+            case d.regularPath.includes('/House Stark/'):
+              return 'rgb(109, 110, 113)';
+            case d.regularPath.includes('/House Targaryen/'):
+              return 'rgb(224, 102, 104)';
+            case d.regularPath.includes('/House Baratheon/'):
+              return 'rgb(153, 135, 123)';
+            case d.regularPath.includes('/House Tyrell/'):
+              return 'rgb(235, 152, 63)';
+            case d.regularPath.includes('/House Lannister/'):
+              return 'rgb(233, 201, 102)';
+            case d.regularPath.includes('/House Tully/'):
+              return 'rgb(87, 181, 194)';
+            default: return '#C28229';
+          }
+        }
+      }
+    }],
     require('./internal-plugin.js')
   ],
   extraWatchFiles: [
